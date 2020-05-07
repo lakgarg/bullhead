6845238ac99de81f529fbaf1da5f97667319fea3
Lakshay Garg <lakshaygarg.in@gmail.com>
2020-05-07 14:52:29 +0000

    Revert "ion: Disable debugging"
    This reverts commit fbee551cb46c4b43f670eb2c97ae5f9a4183d9d2.

--- drivers/staging/android/ion/msm/msm_ion.c
+++ drivers/staging/android/ion/msm/msm_ion.c
@@ -108,7 +108,6 @@ static struct ion_heap_desc ion_heap_meta[] = {
 };
 #endif
 
-#if 0
 static int msm_ion_lowmem_notifier(struct notifier_block *nb,
 					unsigned long action, void *data)
 {
@@ -119,7 +118,6 @@ static int msm_ion_lowmem_notifier(struct notifier_block *nb,
 static struct notifier_block msm_ion_nb = {
 	.notifier_call = msm_ion_lowmem_notifier,
 };
-#endif
 
 struct ion_client *msm_ion_client_create(const char *name)
 {
@@ -1020,10 +1018,7 @@ static int msm_ion_probe(struct platform_device *pdev)
 	 */
 	idev = new_dev;
 
-#if 0
 	show_mem_notifier_register(&msm_ion_nb);
-#endif
-
 	return 0;
 
 freeheaps:
